14d0bdfcd250e27feae185318296d742a68ad7ab
Michał Banaszek <michalbanaszek03@gmail.com>
2023-06-18 19:53:28 +0200

    Program.cpp v1.0

--- Program.cpp
+++ Program.cpp
@@ -5,5 +5,17 @@
 #include "Program.h"
 
 void Program::AddNewClient() {
-
+    string name, surname, address1;
+    char gender1;
+    cout << "Podaj dane klienta do dodania: \n";
+    cout << "Plec: (K/M)" << endl;
+    cin >> gender1;
+    cout << "Imie: " << endl;
+    cin >> name;
+    cout << "Nazwisko: " << endl;
+    cin >> surname;
+    cout << "Adres: " << endl;
+    cin >> address1;
+    Client client(name, surname, address1, gender1);
+    cout << "Klient zostal dodany do bazy danych" << endl;
 }
